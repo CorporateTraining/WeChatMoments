1f179ad45a8a5f4d33d8bf0dcf1219105ab7ccbf
mahuanfy <mahuanfy@foxmail.com>
2020-08-09 22:02:41 +0800

    [Huanhuan]: refactor the code.

--- app/src/androidTest/java/com/example/wechatmoments/repository/WeChatRepositoryTest.java
+++ app/src/androidTest/java/com/example/wechatmoments/repository/WeChatRepositoryTest.java
@@ -1,50 +1,24 @@
-package com.example.wechatmoments.ui;
+package com.example.wechatmoments.repository;
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.example.wechatmoments.repository.WeChatRepositoryImpl;
 import com.example.wechatmoments.repository.data.WeChatData;
 import com.example.wechatmoments.repository.entity.User;
 import com.example.wechatmoments.repository.entity.WeChatMoment;
 import com.example.wechatmoments.repository.entity.vo.Comment;
+import com.example.wechatmoments.ui.WeChatRepository;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import io.reactivex.schedulers.Schedulers;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 
 @RunWith(AndroidJUnit4.class)
 public class WeChatRepositoryTest {
 
     private WeChatRepository weChatRepository = new WeChatRepositoryImpl();
-
-    @Before
-    public void setUp() {
-
-    }
-
-    @Test
-    public void should_find_correct_user1() {
-//        final String CREATE_USER_NAME = "xiaoming";
-//         final Integer CREATE_USER_ID = 1;
-//         final String CREATE_PASSWORD = "123456";
-//        User savedUser = new User(CREATE_USER_ID, CREATE_USER_NAME, Encryptor.md5(CREATE_PASSWORD));
-//        appDatabase.userDBDataSource().save(savedUser).subscribeOn(Schedulers.io()).subscribe();
-//        userRepository.findByName(CREATE_USER_NAME).test()
-//                .assertValue(user -> user.getId() == savedUser.getId());
-    }
 
     @Test
     public void should_find_correct_user() {
